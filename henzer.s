b937648612fe729aa823bf413a7806d7004774ab
Henzer <henzer.g.09@gmail.com>
2015-06-02 17:17:21 -0600

    aaa

--- henzer.s
+++ henzer.s
@@ -15,44 +15,44 @@ suma_0:
 	SUB R0, SP, #4
 	STR R1, [R0]
 
+STARTWHILE1:
 	LDR R0 , [SP, #-4]
-	MOV R1, #10
-	CMP R0, R1
-	BEQ IFTRUE1
-	B IFFALSE1
-IFTRUE1:
-	MOV R2, #1
-	SUB R0, SP, #8
-	STR R2, [R0]
+	MOV R1, #0
+	CMP R0, GOTO
 	B NEXT2
-IFFALSE1:
-	MOV R2, #2
-	SUB R0, SP, #8
-	STR R2, [R0]
-NEXT2:
-	LDR R0 , [SP, #-8]
+WHILETRUE1:
+	LDR R0 , [SP, #-4]
+	MOV R2, #1
+	SUB R3, R0, R2
+	SUB R1, SP, #4
+	STR R3, [R1]
+
+	LDR R1 , [SP, #-4]
 	PUSH {R1, R0}
-	MOV R1, R0
+	MOV R1, R1
 	LDR R0, =$int
 	BL printf
 	POP {R1, R0}
-	POP {R2}
+
+	B STARTWHILE1
+NEXT2:
+	POP {R3}
 	PUSH {R0}
-	MOV PC, R2
+	MOV PC, R3
 
 main_0:
 
 	SUB SP, SP, #0
-
 	LDR R0, =retorno1
 	PUSH {R0}
 	BL suma_0
 retorno1:
-	POP {R2}
+	POP {R3}
 	ADD SP, SP, #0
-	POP {R2}
+
+	POP {R3}
 	PUSH {R0}
-	MOV PC, R2
+	MOV PC, R3
 $ERROR:
 	PUSH {R0}
 	LDR R0, =$indexoutofbounds
