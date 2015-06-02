c23070e9afc584816c98613bc0ae3c69dfa30c70
Henzer <henzer.g.09@gmail.com>
2015-06-02 01:47:02 -0600

    aaa

--- henzer.s
+++ henzer.s
@@ -3,26 +3,30 @@
 .GLOBAL main
 main:
 	STMFD SP!,{LR}
-	SUB SP, SP, #0
-	LDR R0, =$GLOBAL
-	STR SP, [R0]
+	MOV R12, SP
+	SUB SP, SP, #4
 	LDR R0, =$FIN
 	PUSH {R0}
 	BL main_0
 	
 suma_0:
 	MOV R1, #10
-	LDR R0, [SP, #-4]
+	SUB R0, SP, #4
 	STR R1, [R0]
-	LDR R0, [SP, #-4]
+	LDR R1 ,[SP, #-4]
+	MOV R2, #1
+	ADD R3, R1, R2
+	SUB R0, R12, #4
+	STR R3, [R0]
+	LDR R0 ,[R12, #-4]
 	PUSH {R1, R0}
 	MOV R1, R0
 	LDR R0, =$int
 	BL printf
 	POP {R1, R0}
-	POP {R1}
+	POP {R3}
 	PUSH {R0}
-	MOV PC, R1
+	MOV PC, R3
 
 main_0:
 	SUB SP, SP, #0
@@ -30,16 +34,16 @@ main_0:
 	PUSH {R0}
 	BL suma_0
 retorno1:
-	POP {R1}
+	POP {R3}
 	ADD SP, SP, #0
-	POP {R1}
+	POP {R3}
 	PUSH {R0}
-	MOV PC, R1
+	MOV PC, R3
 $FIN:
 	MOV R0,#0
 	MOV R3,#0
 	POP {R0}
-	ADD SP, SP, #0
+	ADD SP, SP, #4
 	LDMFD SP!,{LR}
 	BX LR
 
