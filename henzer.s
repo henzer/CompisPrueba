c9915eddb4ad0404063626aade07581c92b8c973
Henzer <henzer.g.09@gmail.com>
2015-06-02 19:50:17 -0600

    aaa

--- henzer.s
+++ henzer.s
@@ -26,22 +26,44 @@ IFTRUE1:
 
 	B NEXT1
 IFFALSE1:
-	LDR R0 , [SP, #-4]
-	MOV R1, #1
-	SUB R2, R0, R1
+	LDR R1 , [SP, #-4]
+	MOV R2, #1
+	SUB R3, R1, R2
 
-	SUB SP, SP, #4
+	SUB SP, SP, #12
 	LDR R11, =retorno1
 	PUSH {R11}
-	STR R2, [SP, #-4] 
+	STR R3, [SP, #-4] 
 
 	BL fib_0
 retorno1:
+	POP {R2}
+	ADD SP, SP, #12
+	SUB R0, SP, #8
+	STR R2, [R0]
+
+	LDR R2 , [SP, #-4]
+	MOV R1, #2
+	SUB R4, R2, R1
+
+	SUB SP, SP, #12
+	LDR R11, =retorno2
+	PUSH {R11}
+	STR R4, [SP, #-4] 
+
+	BL fib_0
+retorno2:
 	POP {R1}
-	ADD SP, SP, #4
-	POP {R0}
-	PUSH {R1}
-	MOV PC, R0
+	ADD SP, SP, #12
+	SUB R0, SP, #12
+	STR R1, [R0]
+
+	LDR R0 , [SP, #-8]
+	LDR R1 , [SP, #-12]
+	ADD R2, R0, R1
+	POP {R1}
+	PUSH {R2}
+	MOV PC, R1
 
 
 NEXT1:
@@ -50,12 +72,12 @@ main_0:
 	MOV R1, #5
 
 	SUB SP, SP, #4
-	LDR R11, =retorno2
+	LDR R11, =retorno3
 	PUSH {R11}
 	STR R1, [SP, #-4] 
 
 	BL fib_0
-retorno2:
+retorno3:
 	POP {R2}
 	ADD SP, SP, #4
 	SUB R0, SP, #4
