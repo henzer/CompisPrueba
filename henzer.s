19b5bf52e887a1d37df9df5e25992bcba2c77437
Henzer <henzer.g.09@gmail.com>
2015-06-02 19:29:12 -0600

    aaa

--- henzer.s
+++ henzer.s
@@ -11,95 +11,66 @@ main:
 	BL main_0
 	
 fib_0:
+
 	LDR R0 , [SP, #-4]
-	PUSH {R0, R1}
-	MOV R1, R0
-	LDR R0, =$int
-	BL printf
-	POP {R0, R1}
-
-
-	LDR R1 , [SP, #-4]
-	MOV R2, #2
-	CMP R1, R2
+	MOV R1, #2
+	CMP R0, R1
 	BLT IFTRUE1
 	B IFFALSE1
 IFTRUE1:
-	MOV R2, #1
-	POP {R1}
-	PUSH {R2}
-	MOV PC, R1
+	MOV R1, #1
+	POP {R0}
+	PUSH {R1}
+	MOV PC, R0
 
 
-	B NEXT2
+	B NEXT1
 IFFALSE1:
-	LDR R1 , [SP, #-4]
-	MOV R2, #1
-	SUB R4, R1, R2
-
-	SUB SP, SP, #12
-	LDR R11, =retorno1
-	PUSH {R11}
-	STR R4, [SP, #-4] 
-
-	BL fib_0
-retorno1:
-	POP {R2}
-	ADD SP, SP, #12
-	SUB R3, SP, #8
-	STR R2, [R3]
-
 	LDR R2 , [SP, #-4]
-	MOV R1, #2
-	SUB R5, R2, R1
-
-	SUB SP, SP, #12
-	LDR R11, =retorno2
-	PUSH {R11}
-	STR R5, [SP, #-4] 
-
-	BL fib_0
-retorno2:
-	POP {R1}
-	ADD SP, SP, #12
-	SUB R3, SP, #12
-	STR R1, [R3]
-
-	LDR R3 , [SP, #-8]
-	LDR R1 , [SP, #-12]
-	ADD R2, R3, R1
-	POP {R1}
-	PUSH {R2}
-	MOV PC, R1
-
-
-NEXT2:
-
-main_0:
-	MOV R1, #5
+	MOV R0, #1
+	SUB R1, R2, R0
 
 	SUB SP, SP, #4
-	LDR R11, =retorno3
+	LDR R11, =retorno1
 	PUSH {R11}
 	STR R1, [SP, #-4] 
 
 	BL fib_0
-retorno3:
-	POP {R2}
+retorno1:
+	POP {R0}
 	ADD SP, SP, #4
-	SUB R3, SP, #4
-	STR R2, [R3]
+	POP {R0}
+	PUSH {R0}
+	MOV PC, R0
 
-	LDR R3 , [SP, #-4]
+
+NEXT1:
+
+main_0:
+	MOV R0, #5
+
+	SUB SP, SP, #4
+	LDR R11, =retorno2
+	PUSH {R11}
+	STR R0, [SP, #-4] 
+
+	BL fib_0
+retorno2:
+	POP {R3}
+	ADD SP, SP, #4
+	SUB R2, SP, #4
+	STR R3, [R2]
+
+	LDR R2 , [SP, #-4]
 	PUSH {R0, R1}
-	MOV R1, R3
+	MOV R1, R2
 	LDR R0, =$int
 	BL printf
 	POP {R0, R1}
 
-	POP {R2}
+	POP {R3}
 	PUSH {R0}
-	MOV PC, R2
+	MOV PC, R3
 $ERROR:
 	PUSH {R0}
 	LDR R0, =$indexoutofbounds
