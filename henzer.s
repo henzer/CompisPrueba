cb90f0298073635b62f2897a9a2829b06c814886
Henzer <henzer.g.09@gmail.com>
2015-06-02 19:47:38 -0600

    aaa

--- henzer.s
+++ henzer.s
@@ -26,51 +26,51 @@ IFTRUE1:
 
 	B NEXT1
 IFFALSE1:
-	LDR R2 , [SP, #-4]
-	MOV R0, #1
-	SUB R1, R2, R0
+	LDR R0 , [SP, #-4]
+	MOV R1, #1
+	SUB R2, R0, R1
 
 	SUB SP, SP, #4
 	LDR R11, =retorno1
 	PUSH {R11}
-	STR R1, [SP, #-4] 
+	STR R2, [SP, #-4] 
 
 	BL fib_0
 retorno1:
-	POP {R0}
+	POP {R1}
 	ADD SP, SP, #4
 	POP {R0}
-	PUSH {R0}
+	PUSH {R1}
 	MOV PC, R0
 
 
 NEXT1:
 
 main_0:
-	MOV R0, #5
+	MOV R1, #5
 
 	SUB SP, SP, #4
 	LDR R11, =retorno2
 	PUSH {R11}
-	STR R0, [SP, #-4] 
+	STR R1, [SP, #-4] 
 
 	BL fib_0
 retorno2:
-	POP {R3}
+	POP {R2}
 	ADD SP, SP, #4
-	SUB R2, SP, #4
-	STR R3, [R2]
+	SUB R0, SP, #4
+	STR R2, [R0]
 
-	LDR R2 , [SP, #-4]
+	LDR R0 , [SP, #-4]
 	PUSH {R0, R1}
-	MOV R1, R2
+	MOV R1, R0
 	LDR R0, =$int
 	BL printf
 	POP {R0, R1}
 
-	POP {R3}
+	POP {R2}
 	PUSH {R0}
-	MOV PC, R3
+	MOV PC, R2
 $ERROR:
 	PUSH {R0}
 	LDR R0, =$indexoutofbounds
