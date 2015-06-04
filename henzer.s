d870e96a49e601098b762e7b6bbfb8026f2bce2d
Henzer <henzer.g.09@gmail.com>
2015-06-04 16:06:37 -0600

    aaa

--- henzer.s
+++ henzer.s
@@ -69,43 +69,40 @@ retorno2:
 NEXT1:
 
 main_0:
-	MOV R1, #10
-	SUB R0, SP, #8
-	STR R1, [R0]
+	MOV R1, #1
 
-STARTWHILE1:
-	LDR R0 , [SP, #-8]
-	MOV R1, #0
-	CMP R0, R1
-	BGT WHILETRUE1
-	B NEXT7
-WHILETRUE1:
-	LDR R0 , [SP, #-8]
-	MOV R2, #1
-	SUB R3, R0, R2
-	SUB R1, SP, #8
-	STR R3, [R1]
-
-	LDR R3 , [SP, #-8]
-
-	SUB SP, SP, #8
+	SUB SP, SP, #4
 	LDR R11, =retorno3
 	PUSH {R11}
-	STR R3, [SP, #-4] 
+	STR R1, [SP, #-4] 
 
 	BL fib_0
 retorno3:
 	POP {R2}
-	ADD SP, SP, #8
-	SUB R1, SP, #4
-	STR R2, [R1]
+	ADD SP, SP, #4
+	SUB R0, SP, #4
+	STR R2, [R0]
 
-	LDR R1 , [SP, #-8]
+	LDR R0 , [SP, #-4]
 	PUSH {R0, R1}
-	MOV R1, R1
+	MOV R1, R0
 	LDR R0, =$int
 	BL printf
 	POP {R0, R1}
+
+	MOV R3, #2
+
+	SUB SP, SP, #4
+	LDR R11, =retorno4
+	PUSH {R11}
+	STR R3, [SP, #-4] 
+
+	BL fib_0
+retorno4:
+	POP {R4}
+	ADD SP, SP, #4
+	SUB R2, SP, #4
+	STR R4, [R2]
 
 	LDR R2 , [SP, #-4]
 	PUSH {R0, R1}
@@ -114,11 +111,72 @@ retorno3:
 	BL printf
 	POP {R0, R1}
 
-	B STARTWHILE1
-NEXT7:
-	POP {R0}
+	MOV R5, #3
+
+	SUB SP, SP, #4
+	LDR R11, =retorno5
+	PUSH {R11}
+	STR R5, [SP, #-4] 
+
+	BL fib_0
+retorno5:
+	POP {R6}
+	ADD SP, SP, #4
+	SUB R4, SP, #4
+	STR R6, [R4]
+
+	LDR R4 , [SP, #-4]
+	PUSH {R0, R1}
+	MOV R1, R4
+	LDR R0, =$int
+	BL printf
+	POP {R0, R1}
+
+	MOV R7, #4
+
+	SUB SP, SP, #4
+	LDR R11, =retorno6
+	PUSH {R11}
+	STR R7, [SP, #-4] 
+
+	BL fib_0
+retorno6:
+	POP {R8}
+	ADD SP, SP, #4
+	SUB R6, SP, #4
+	STR R8, [R6]
+
+	LDR R6 , [SP, #-4]
+	PUSH {R0, R1}
+	MOV R1, R6
+	LDR R0, =$int
+	BL printf
+	POP {R0, R1}
+
+	MOV R9, #5
+
+	SUB SP, SP, #4
+	LDR R11, =retorno7
+	PUSH {R11}
+	STR R9, [SP, #-4] 
+
+	BL fib_0
+retorno7:
+	POP {R10}
+	ADD SP, SP, #4
+	SUB R8, SP, #4
+	STR R10, [R8]
+
+	LDR R8 , [SP, #-4]
+	PUSH {R0, R1}
+	MOV R1, R8
+	LDR R0, =$int
+	BL printf
+	POP {R0, R1}
+
+	POP {R10}
 	PUSH {R0}
-	MOV PC, R0
+	MOV PC, R10
 $ERROR:
 	PUSH {R0}
 	LDR R0, =$indexoutofbounds
