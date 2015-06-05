9ca7b3d4cfe2e604338994e147e23bad5ce2328f
Henzer <henzer.g.09@gmail.com>
2015-06-04 19:29:54 -0600

    aaa

--- henzer.s
+++ henzer.s
@@ -10,136 +10,36 @@ main:
 	PUSH {R0}
 	BL main_0
 	
-factorial_0:
-
-	LDR R0 , [SP, #-4]
-	MOV R1, #2
-	CMP R0, R1
-	BLT IFTRUE1
-	B IFFALSE1
-IFTRUE1:
-	MOV R1, #1
-	POP {R0}
-	PUSH {R1}
-	MOV PC, R0
-
-
-	B NEXT1
-IFFALSE1:
-	LDR R1 , [SP, #-4]
-	MOV R2, #1
-	SUB R3, R1, R2
-
-	SUB SP, SP, #8
-	LDR R11, =retorno1
-	PUSH {R11}
-	STR R3, [SP, #-4] 
-
-	BL factorial_0
-retorno1:
-	POP {R2}
-	ADD SP, SP, #8
-	SUB R0, SP, #8
-	STR R2, [R0]
-
-	LDR R0 , [SP, #-8]
-	LDR R2 , [SP, #-4]
-	MUL R1, R0, R2
-	POP {R2}
-	PUSH {R1}
-	MOV PC, R2
-
-
-NEXT1:
-
 main_0:
-	MOV R1, #5
-
-	SUB SP, SP, #4
-	LDR R11, =retorno2
-	PUSH {R11}
-	STR R1, [SP, #-4] 
-
-	BL factorial_0
-retorno2:
-	POP {R2}
-	ADD SP, SP, #4
+	MOV R1, #10
 	SUB R0, SP, #4
-	STR R2, [R0]
+	STR R1, [R0]
 
+STARTWHILE1:
 	LDR R0 , [SP, #-4]
+	MOV R1, #0
+	CMP R0, R1
+	BGT WHILETRUE1
+	B NEXT2
+WHILETRUE1:
+	LDR R1 , [SP, #-4]
 	PUSH {R0, R1}
-	MOV R1, R0
+	MOV R1, R1
 	LDR R0, =$int
 	BL printf
 	POP {R0, R1}
-
-	MOV R3, #4
-
-	SUB SP, SP, #4
-	LDR R11, =retorno3
-	PUSH {R11}
-	STR R3, [SP, #-4] 
-
-	BL factorial_0
-retorno3:
-	POP {R4}
-	ADD SP, SP, #4
-	SUB R2, SP, #4
-	STR R4, [R2]
 
 	LDR R2 , [SP, #-4]
-	PUSH {R0, R1}
-	MOV R1, R2
-	LDR R0, =$int
-	BL printf
-	POP {R0, R1}
+	MOV R3, #1
+	SUB R4, R2, R3
+	SUB R0, SP, #4
+	STR R4, [R0]
 
-	MOV R5, #3
-
-	SUB SP, SP, #4
-	LDR R11, =retorno4
-	PUSH {R11}
-	STR R5, [SP, #-4] 
-
-	BL factorial_0
-retorno4:
-	POP {R6}
-	ADD SP, SP, #4
-	SUB R4, SP, #4
-	STR R6, [R4]
-
-	LDR R4 , [SP, #-4]
-	PUSH {R0, R1}
-	MOV R1, R4
-	LDR R0, =$int
-	BL printf
-	POP {R0, R1}
-
-	MOV R7, #2
-
-	SUB SP, SP, #4
-	LDR R11, =retorno5
-	PUSH {R11}
-	STR R7, [SP, #-4] 
-
-	BL factorial_0
-retorno5:
-	POP {R8}
-	ADD SP, SP, #4
-	SUB R6, SP, #4
-	STR R8, [R6]
-
-	LDR R6 , [SP, #-4]
-	PUSH {R0, R1}
-	MOV R1, R6
-	LDR R0, =$int
-	BL printf
-	POP {R0, R1}
-
-	POP {R8}
+	B STARTWHILE1
+NEXT2:
+	POP {R0}
 	PUSH {R0}
-	MOV PC, R8
+	MOV PC, R0
 $ERROR:
 	PUSH {R0}
 	LDR R0, =$indexoutofbounds
