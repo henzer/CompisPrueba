4f7ab9eac236f2059447b8f09cf91d7b9dc2a88d
Henzer <henzer.g.09@gmail.com>
2015-06-04 18:29:28 -0600

    aaa

--- henzer.s
+++ henzer.s
@@ -12,35 +12,35 @@ main:
 	
 fib_0:
 
-	LDR R0 , [SP, #-12]
+	LDR R0 , [SP, #-4]
 	MOV R1, #2
 	CMP R0, R1
 	BLT IFTRUE1
 	B IFFALSE1
 IFTRUE1:
 	MOV R1, #1
-	LDR R0, [ SP , #-4 ]
-	STR R1, [SP, #-8]
+	POP {R0}
+	PUSH {R1}
 	MOV PC, R0
 
 
 	B NEXT1
 IFFALSE1:
-	LDR R0 , [SP, #-12]
+	LDR R0 , [SP, #-4]
 	MOV R1, #1
 	SUB R2, R0, R1
 
-	SUB SP, SP, #12
+	SUB SP, SP, #4
 	LDR R11, =retorno1
-	STR R11, [SP, #-4]
-	STR R2, [SP, #-12] 
+	PUSH {R11}
+	STR R2, [SP, #-4] 
 
 	BL fib_0
 retorno1:
-	LDR R1, [SP, #-8]
-	ADD SP, SP, #12
-	LDR R0, [ SP , #-4 ]
-	STR R1, [SP, #-8]
+	POP {R1}
+	ADD SP, SP, #4
+	POP {R0}
+	PUSH {R1}
 	MOV PC, R0
 
 
@@ -48,27 +48,27 @@ NEXT1:
 
 main_0:
 	MOV R1, #5
-	SUB R0, SP, #16
+	SUB R0, SP, #8
 	STR R1, [R0]
 
 	MOV R1, #0
-	SUB R0, SP, #12
+	SUB R0, SP, #4
 	STR R1, [R0]
 
 STARTWHILE1:
-	LDR R0 , [SP, #-12]
+	LDR R0 , [SP, #-4]
 	MOV R1, #10
 	CMP R0, R1
 	BLT WHILETRUE1
 	B NEXT6
 WHILETRUE1:
-	LDR R0 , [SP, #-12]
+	LDR R0 , [SP, #-4]
 	MOV R2, #1
 	ADD R3, R0, R2
-	SUB R1, SP, #12
+	SUB R1, SP, #4
 	STR R3, [R1]
 
-	LDR R1 , [SP, #-12]
+	LDR R1 , [SP, #-4]
 	PUSH {R0, R1}
 	MOV R1, R1
 	LDR R0, =$int
@@ -76,27 +76,27 @@ WHILETRUE1:
 	POP {R0, R1}
 
 
-	LDR R3 , [SP, #-12]
+	LDR R3 , [SP, #-4]
 	MOV R2, #5
 	CMP R3, R2
 	BEQ IFTRUE2
 	B NEXT9
 IFTRUE2:
-	LDR R0 , [SP, #-12]
+	LDR R0 , [SP, #-4]
 
-	SUB SP, SP, #16
+	SUB SP, SP, #8
 	LDR R11, =retorno2
-	STR R11, [SP, #-4]
-	STR R0, [SP, #-12] 
+	PUSH {R11}
+	STR R0, [SP, #-4] 
 
 	BL fib_0
 retorno2:
-	LDR R4, [SP, #-8]
-	ADD SP, SP, #16
-	SUB R3, SP, #16
+	POP {R4}
+	ADD SP, SP, #8
+	SUB R3, SP, #8
 	STR R4, [R3]
 
-	LDR R3 , [SP, #-16]
+	LDR R3 , [SP, #-8]
 	PUSH {R0, R1}
 	MOV R1, R3
 	LDR R0, =$int
@@ -107,8 +107,8 @@ retorno2:
 NEXT9:
 	B STARTWHILE1
 NEXT6:
-	LDR R4, [ SP , #-4 ]
-	STR R0, [SP, #-8]
+	POP {R4}
+	PUSH {R0}
 	MOV PC, R4
 $ERROR:
 	PUSH {R0}
