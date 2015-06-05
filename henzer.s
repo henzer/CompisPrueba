49d27414d3a173a31cba77d6b03e646174c57533
Henzer <henzer.g.09@gmail.com>
2015-06-04 18:55:36 -0600

    aaa

--- henzer.s
+++ henzer.s
@@ -10,94 +10,106 @@ main:
 	PUSH {R0}
 	BL main_0
 	
-fib_0:
-
+suma_0:
 	LDR R0 , [SP, #-4]
-	MOV R1, #2
-	CMP R0, R1
-	BLT IFTRUE1
-	B IFFALSE1
-IFTRUE1:
-	MOV R1, #1
-	POP {R0}
-	PUSH {R1}
-	MOV PC, R0
-
-
-	B NEXT1
-IFFALSE1:
-	LDR R1 , [SP, #-4]
-	MOV R2, #1
-	SUB R3, R1, R2
-
-	SUB SP, SP, #12
-	LDR R11, =retorno1
-	PUSH {R11}
-	STR R3, [SP, #-4] 
-
-	BL fib_0
-retorno1:
-	POP {R2}
-	ADD SP, SP, #12
-	SUB R0, SP, #8
-	STR R2, [R0]
-
-	LDR R2 , [SP, #-4]
-	MOV R1, #2
-	SUB R4, R2, R1
-
-	SUB SP, SP, #12
-	LDR R11, =retorno2
-	PUSH {R11}
-	STR R4, [SP, #-4] 
-
-	BL fib_0
-retorno2:
-	POP {R1}
-	ADD SP, SP, #12
-	SUB R0, SP, #12
-	STR R1, [R0]
-
-	LDR R0 , [SP, #-8]
-	LDR R1 , [SP, #-12]
+	LDR R1 , [SP, #-8]
 	ADD R2, R0, R1
 	POP {R1}
 	PUSH {R2}
 	MOV PC, R1
 
 
-NEXT1:
+restar_0:
+	LDR R0 , [SP, #-4]
+	LDR R1 , [SP, #-8]
+	SUB R2, R0, R1
+	POP {R1}
+	PUSH {R2}
+	MOV PC, R1
+
+
+mult_0:
+	LDR R0 , [SP, #-4]
+	LDR R1 , [SP, #-8]
+	MUL R2, R0, R1
+	POP {R1}
+	PUSH {R2}
+	MOV PC, R1
+
 
 main_0:
-	MOV R1, #10
-	SUB R0, SP, #4
-	STR R1, [R0]
+	MOV R1, #100
+	MOV R2, #50
 
-STARTWHILE1:
+	SUB SP, SP, #4
+	LDR R11, =retorno1
+	PUSH {R11}
+	STR R1, [SP, #-4] 
+	STR R2, [SP, #-8] 
+
+	BL suma_0
+retorno1:
+	POP {R3}
+	ADD SP, SP, #4
+	SUB R0, SP, #4
+	STR R3, [R0]
+
 	LDR R0 , [SP, #-4]
-	MOV R1, #0
-	CMP R0, R1
-	BGT WHILETRUE1
-	B NEXT7
-WHILETRUE1:
-	LDR R1 , [SP, #-4]
 	PUSH {R0, R1}
-	MOV R1, R1
+	MOV R1, R0
 	LDR R0, =$int
 	BL printf
 	POP {R0, R1}
 
-	LDR R2 , [SP, #-4]
-	MOV R3, #1
-	SUB R4, R2, R3
-	SUB R0, SP, #4
-	STR R4, [R0]
+	MOV R4, #100
+	MOV R5, #50
 
-	B STARTWHILE1
-NEXT7:
-	POP {R0}
+	SUB SP, SP, #4
+	LDR R11, =retorno2
+	PUSH {R11}
+	STR R4, [SP, #-4] 
+	STR R5, [SP, #-8] 
+
+	BL restar_0
+retorno2:
+	POP {R6}
+	ADD SP, SP, #4
+	SUB R3, SP, #4
+	STR R6, [R3]
+
+	LDR R3 , [SP, #-4]
+	PUSH {R0, R1}
+	MOV R1, R3
+	LDR R0, =$int
+	BL printf
+	POP {R0, R1}
+
+	MOV R7, #5
+	MOV R8, #50
+
+	SUB SP, SP, #4
+	LDR R11, =retorno3
+	PUSH {R11}
+	STR R7, [SP, #-4] 
+	STR R8, [SP, #-8] 
+
+	BL mult_0
+retorno3:
+	POP {R9}
+	ADD SP, SP, #4
+	SUB R6, SP, #4
+	STR R9, [R6]
+
+	LDR R6 , [SP, #-4]
+	PUSH {R0, R1}
+	MOV R1, R6
+	LDR R0, =$int
+	BL printf
+	POP {R0, R1}
+
+	POP {R9}
 	PUSH {R0}
-	MOV PC, R0
+	MOV PC, R9
 $ERROR:
 	PUSH {R0}
 	LDR R0, =$indexoutofbounds
